7c09674a44e5d49382f819d6e24715cf25eb21d9
NovikVeronika <veronika.novik79@mail.ru>
2022-05-17 01:11:29 +0300

    add images change

--- js/startTime.js
+++ js/startTime.js
@@ -2,52 +2,66 @@
 //export let startButton = document.getElementById("startTimeButton")
 var secondsRemaining;
 var intervalHandle;
-var timeDisplay = document.getElementById("time-screen");
+var timeDisplay = document.getElementById("time-screen")
+var minutes = document.getElementById("time").value;
 
 function invertTime() {
-    // turning the seconds into mm:ss
-    var min = Math.floor(secondsRemaining / 60);
-    var sec = secondsRemaining - (min * 60);
 
-    //adding a leading zero ( as a string value) if seconds less than 10
+    var min = Math.floor(secondsRemaining / 60)
+    var sec = secondsRemaining - (min * 60)
+    const counter = minutes * 60
+    const count1 = 0.8 * counter
+    const count2 = 0.6 * counter
+    const count3 = 0.4 * counter
+    const count4 = 0.2 * counter
+
     if (sec < 10) {
         sec = "0" + sec;
     }
-    //concatenating with colon
 
     var message = min + ":" + sec;
-    //changing the display
+
     timeDisplay.innerHTML = message;
+    var image = document.getElementById("image");
+    var images = ["../images/1.png", "../images/2.png", "../images/3.png","../images/4.png","../images/5.png"]
 
-    // stop if counter gets to zero
-    if (secondsRemaining === 0) {
-        alert("DONE");
-        clearInterval(intervalHandle);
-        document.getElementById("time").style.display = "block";
+    if(secondsRemaining === counter){
+        image.src = images[0];
     }
-    // substracting from seconds remaining
-    secondsRemaining--;
+    else if(secondsRemaining === count1){
+        image.src = images[1];
+    }
+    else if(secondsRemaining === count2){
+        image.src = images[2];
+    }
+    else if(secondsRemaining === count3){
+        image.src = images[3];
+    }
+    else if(secondsRemaining === count4){
+        image.src = images[4];
+    }
 
+    if (secondsRemaining === 0) {
+        image.src = images[4]
+        alert("DONE! YOU ARE AMAZING!!! YOU MADE IT!!!!")
+        clearInterval(intervalHandle)
+        document.getElementById("time").style.display = "none"
+        document.getElementById("motivation-label").style.display = "none"
+    }
+    secondsRemaining--;
 }
 
 function startCountdown() {
-    // getting contents of the minutes box
-    var minutes = document.getElementById("time").value;
 
-    // checking if input is not a number
     if (isNaN(minutes)) {
         alert("please enter a number");
         return;
     }
-    //to pick how many seconds
-
     secondsRemaining = minutes * 60;
-    // for every second, call the tick function
     intervalHandle = setInterval(invertTime,1000);
-    // Then Hide the form
+
     document.getElementById("time").style.display = "none";
     timeDisplay.style.display="block";
-
 }
 
 
@@ -56,12 +70,14 @@ window.onload= function () {
     let inputTime = document.getElementById("time")
     let startButton = document.getElementById("startTimeButton")
     let giveUpButton = document.getElementById("giveUpButton")
+    let labels = document.getElementById("label-for-time");
     timeDisplay.style.display="none"
     giveUpButton.style.display="none"
 
     startButton.onclick = function () {
         startButton.style.display="none";
         giveUpButton.style.display="block"
+        labels.style.display="none";
         startCountdown();
     }
 
